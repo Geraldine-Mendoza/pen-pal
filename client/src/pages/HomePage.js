2e8da27d4ec3f15816238e1ae5c34209ed37b1e5
geraldine <hypatia.of.alexandria55@gmail.com>
2021-05-19 20:41:03 -0400

    homepage/navbar basic

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+function HomePage() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  return (
+    <div>
+      <h3>homepage</h3>
+    </div>
+  )
+}
